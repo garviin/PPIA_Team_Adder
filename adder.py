247f16dfe4058fa0dc2315d53711b8ffddf3de6e
garviin <garvibulkin@gmail.com>
2019-09-14 21:43:10 +1000

    implemented json reader

--- adder.py
+++ adder.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+import json
+
 # Functions to implement
 # 1. Adding a team member on the ppia website
 # 2. Parse filename to tuple (Name, Position)
@@ -9,3 +12,11 @@ Process:
 2. Open a headless chrome browser, login to admin panel, open add teams
 3. Add each image file as a team member (Name & position on filename, division from directory)
 """
+
+# Loads admin.json into tuple (username, password, link)
+def read_admin_info():
+    with open('admin.json') as json_file:
+        data = json.load(json_file)
+        return (data['username'], data['password'], data['link'])
+
+
